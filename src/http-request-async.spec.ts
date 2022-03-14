86fd25729f6620a5ee8af3b9f1582ac189fe71d3
Dongsu Jang <77693+iolo@users.noreply.github.com>
2022-03-14 23:56:33 +0900

    add more test cases for 404

--- src/http-request-async.spec.ts
+++ src/http-request-async.spec.ts
@@ -5,14 +5,50 @@ describe('http-request-async', () => {
     it('should do GET', async () => {
       const TEST_URL = 'http://httpbin.org/get';
       const res = await httpRequestAsync(TEST_URL);
+      expect(res.statusCode).toBe(200);
       const data = JSON.parse(res.data);
       expect(data.url).toBe(TEST_URL);
+    });
+    it('should do GET with URL', async () => {
+      const TEST_URL = 'http://httpbin.org/get';
+      const res = await httpRequestAsync(new URL(TEST_URL));
+      expect(res.statusCode).toBe(200);
+      const data = JSON.parse(res.data);
+      expect(data.url).toBe(TEST_URL);
+    });
+    it('should do GET 404', async () => {
+      const TEST_URL = 'http://httpbin.org/status/404';
+      const res = await httpRequestAsync(TEST_URL);
+      expect(res.statusCode).toBe(404);
     });
     it('should do GET via https', async () => {
       const TEST_URL = 'https://httpbin.org/get';
       const res = await httpRequestAsync(TEST_URL);
       const data = JSON.parse(res.data);
       expect(data.url).toBe(TEST_URL);
+    });
+    it('should do GET with URL via https', async () => {
+      const TEST_URL = 'https://httpbin.org/get';
+      const res = await httpRequestAsync(new URL(TEST_URL));
+      const data = JSON.parse(res.data);
+      expect(data.url).toBe(TEST_URL);
+    });
+    it('should do GET with options.port via https', async () => {
+      const TEST_URL = 'https://httpbin.org/get';
+      const res = await httpRequestAsync({ host: 'httpbin.org', path: '/get', port: 443 });
+      const data = JSON.parse(res.data);
+      expect(data.url).toBe(TEST_URL);
+    });
+    it('should do GET with options.protocol via https', async () => {
+      const TEST_URL = 'https://httpbin.org/get';
+      const res = await httpRequestAsync({ host: 'httpbin.org', path: '/get', protocol: 'https:' });
+      const data = JSON.parse(res.data);
+      expect(data.url).toBe(TEST_URL);
+    });
+    it('should do GET 404 via https', async () => {
+      const TEST_URL = 'https://httpbin.org/status/404';
+      const res = await httpRequestAsync(TEST_URL);
+      expect(res.statusCode).toBe(404);
     });
   });
 });

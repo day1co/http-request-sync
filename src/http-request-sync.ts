6c88299e535f4c065d253f203f0fe5bacf2b1778
moonimooni <heewonmoon@day1company.co.kr>
2022-04-06 18:48:09 +0900

    fix: apply try-catch phrase to httpRequestSync worker

--- src/http-request-sync.ts
+++ src/http-request-sync.ts
@@ -45,7 +45,8 @@ function resolve(res, data) {
 }
 
 function reject(error) {
-  port.postMessage(JSON.stringify({ error }));
+  const { stack } = error;
+  port.postMessage(JSON.stringify({ error: stack }));
   notify();
 }
 
@@ -57,38 +58,42 @@ const isHttps =
   (typeof options === 'object' && (options.port === 443 || options.protocol === 'https:'));
 const request = isHttps ? https.request : http.request;
 
-const req = request(options, (res) => {
-  log('statusCode:', res.statusCode);
-  log('headers:', res.headers);
-  res.setEncoding('utf8');
-  let data = '';
-  res.on('data', (chunk) => {
-    log('>' + chunk);
-    data += chunk;
+try {
+  const req = request(options, (res) => {
+    log('statusCode:', res.statusCode);
+    log('headers:', res.headers);
+    res.setEncoding('utf8');
+    let data = '';
+    res.on('data', (chunk) => {
+      log('>' + chunk);
+      data += chunk;
+    });
+    res.on('error', (error) => {
+      return reject(error);
+    });
+    res.on('end', () => {
+      return resolve(res, data);
+    });
   });
-  res.on('error', (error) => {
+  
+  req.on('error', (error) => {
+    log('error!', error, options, typeof options, options instanceof URL);
     return reject(error);
   });
-  res.on('end', () => {
-    return resolve(res, data);
+  req.on('timeout', () => {
+    log('timeout!');
+    return reject();
   });
-});
-
-req.on('error', (error) => {
-  log('error!', error, options, typeof options, options instanceof URL);
-  return reject(error);
-});
-req.on('timeout', () => {
-  log('timeout!');
-  return reject();
-});
-req.on('uncaughtException', (error) => {
-  log('uncaughtException!', error);
-  return reject(error);
-});
-req.end(() => {
-  log('end!');
-});
+  req.on('uncaughtException', (error) => {
+    log('uncaughtException!', error);
+    return reject(error);
+  });
+  req.end(() => {
+    log('end!');
+  });
+} catch (err) {
+  reject(err);
+}
   `,
     {
       eval: true,
